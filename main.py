fe5178263b34cbd9e4b10fefddfb3f03043d1f70
rupakdeo <deo.rupak90@gmail.com>
2021-06-04 07:13:09 +0545

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+a =("rupak ,deo")
+a.split(",")
+print (a)
+#sppp
+b = ("-this-house-is-greeen")
+b.split(" ")
+print(a + b)
+#
+my_employee = ("i am a employee")
+rupak = my_employee.replace("i","you")
+print(rupak)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+### join
+rupak1 = ("you","are","a","boy")
+rupak2 = (" ").join(rupak1)
+rupak3 = rupak2.upper()
+rupak4 = rupak3.lower()
+print(rupak4)
+print(rupak3[0:6])
+##list 1 append
+xl = ["rupak","orange",3-5,"grapes"]
+xl.append("apple")
+print(xl)
+xz = ["nano" ,"tata"]
+xl.append([xz])
+print(xl)
+##pop
+print(xz)
+x2 = ["car","bus", "truck","4"]
+x2[1] = x2.pop()
+print(x2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/t
+
+
+
